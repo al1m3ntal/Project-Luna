7ff1db043e964e7e50378813db5a758370b85bc9
al1m3ntal <al1m3ntal@users.noreply.github.com>
2014-09-15 00:01:59 +0200

    Update Main.java

--- Project-Luna/src/ui/Main.java
+++ Project-Luna/src/ui/Main.java
@@ -1,12 +1,13 @@
 package ui;
+import java.util.Scanner;
 
 public class Main {
 
 	
 	public static void main(String[] args) {
 		System.out.println("Enter name:");
-			Scanner sc = new Scanner(System.in);
-			String name = sc.next();
-			System.out.println("hello, " + name);
+		Scanner sc = new Scanner(System.in);
+		String name = sc.next();
+		System.out.println("hello, " + name);
 	}
 }
