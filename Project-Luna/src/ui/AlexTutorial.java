1b41f7f1feb3b4d4dfe206c36ff6b18e70e44e73
al1m3ntal <alex.goosen@yahoo.de>
2014-09-16 18:35:13 +0200

    Added libraries

--- Project-Luna/src/ui/AlexTutorial.java
+++ Project-Luna/src/ui/AlexTutorial.java
@@ -1,10 +1,35 @@
 package ui;
 
+import org.lwjgl.LWJGLException;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
+
+
 public class AlexTutorial {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		System.err.println("STARTING TUTORIAL PROGRAMM");
+
+		// set up display
+		try{
+			Display.setDisplayMode(new DisplayMode(1920, 1080));
+			Display.create();
+		} catch(LWJGLException e){
+			e.printStackTrace();
+			System.exit(0);
+		}
+		
+		
+		while (!Display.isCloseRequested()){
+			// reder openGL here
+			Display.update();
+			
+		}
+		
+		
+		Display.destroy();
+		
 	}
 
 }
