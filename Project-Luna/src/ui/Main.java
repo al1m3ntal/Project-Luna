61dcc58068a9ea41110d2f6e9221e23844af57a0
Atlan <atlandg@gmail.com>
2014-09-16 18:23:12 +0200

    Changed Main to init a OpenGl Window and load a texture. Uploaded the texture as well.

--- Project-Luna/src/ui/Main.java
+++ Project-Luna/src/ui/Main.java
@@ -1,41 +1,65 @@
 package ui;
-import java.util.Scanner;
+
+
+import java.io.IOException;
+
+import org.lwjgl.LWJGLException;
+import org.lwjgl.input.Mouse;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
+import org.newdawn.slick.opengl.Texture;
+import org.newdawn.slick.opengl.TextureLoader;
+import org.newdawn.slick.util.ResourceLoader;
 
 public class Main {
 	
+	static Texture texture;
+	
 	public static void main(String[] args) {
-		//Greet the user:
-		printStars(20);
-		sayHello();
-		printStars(20);
+		
+		try {
+			Display.setDisplayMode(new DisplayMode(1000,1000));
+			Display.create();
+		} catch (LWJGLException e) {
+			e.printStackTrace();
+		}
+		
+		init();
+		
+		// init OpenGL here
+		while (!Display.isCloseRequested()) {
+		// render OpenGL here
+			pollInput();
+			
+		Display.update();		
+		}
+		
+		Display.destroy();
+		
 	}
 	
+	public static void pollInput() {
+		if (Mouse.isButtonDown(0)) {
+		int x = Mouse.getX();
+		int y = Mouse.getY();
+		System.out.println("MOUSE DOWN @ X: " + x + " Y: " + y);
+		}
+	}
 	
-	private static void sayHello()	{
 
-		System.out.println("Hello!\n");
-		// User enter his name
-		System.out.print("Enter name: ");
-		Scanner sc = new Scanner(System.in);
-		// gets the line entered by the user
-		String name = sc.next();
-		// output welcome message
-		System.out.println("Hallo, " + name + "!");
-		// close the input stream
-		sc.close();
-	
-	}
 
 	/**
-	 * Prints a line of stars.
-	 * @param stars Nr. of stars to print 
-	 */
-	private static void printStars(int stars){
-		System.out.println();
-		for (int i = 0; i < stars ; i++) {
-			System.out.print("*");
-		}
-		System.out.println();
+	* Initialise resources
+	*/
+	public static void init() {
+	 
+	try {
+	// load texture from PNG file
+	texture = TextureLoader.getTexture("PNG", ResourceLoader.getResourceAsStream("res/desert.png"));
+	 System.out.println(texture.getHeight());
+	} catch (IOException e) {
+	e.printStackTrace();
+	}
 	}
 	
 }
