bbfc4f86b301390bdeb2cfb5bdb0f9eeaed405e3
al1m3ntal <alex.goosen@yahoo.de>
2014-09-17 17:58:24 +0200

    Fixed a bug in Tile class

--- Project-Luna/src/model/Tile.java
+++ Project-Luna/src/model/Tile.java
@@ -5,7 +5,7 @@ import org.lwjgl.util.vector.Vector2f;
 public class Tile {
 
 	public TileType type;
-	public Vector2f position;
+	public Vector2f position = new Vector2f();
 
 	public Tile(TileType tileType, int x , int y){
 		this.type = tileType;
