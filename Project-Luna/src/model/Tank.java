76557aec69ba767c0bfec3aee245c99263e2b26f
al1m3ntal <alex.goosen@yahoo.de>
2014-09-17 17:15:28 +0200

    Udpated tank class (speed is now a vector)

--- Project-Luna/src/model/Tank.java
+++ Project-Luna/src/model/Tank.java
@@ -13,23 +13,23 @@ import org.newdawn.slick.opengl.Texture;
  */
 public class Tank {
 	
-	// Current SPEED of this tank
-	private int speed;
+	// Current SPEED of this tank as represented by a 2D vector 
+	public Vector2f speed;
 	// Current Hit Points of this tank
-	private int hp;
+	public int hp;
 	// Current TURRET ROTATION of this tank
-	private float rotTurret;
+	public float rotTurret;
 	// Current BASE ROTATION (tank) of this tank
-	private float rotBase;
+	public float rotBase;
 	// 300x300px Texture for the tank BASE 
-	private Texture texBase;
+	public Texture texBase;
 	// 300x300px Texture for the tank TURRET
-	private Texture texTurret;
+	public Texture texTurret;
 	// Current tank position as represented by a 2D vector
 	// NOTE: NOT VECTOR2F FROM SLICK LIBRARY
-	private Vector2f position;
+	public Vector2f position;
 	// Tank stats that is used by this tank 
-	private TankStats stats;
+	public TankStats stats;
 
 	
 	/**
@@ -40,22 +40,5 @@ public class Tank {
 		this.stats = stats;
 	}
 
-	
-	/* GETTERS AND SETTERS */
-	public void setSpeed(int speed){ this.speed = speed;	}
-	public int getSpeed() {	return speed; }
-	public int getHp() { return hp; }
-	public void setHp(int hp) {	this.hp = hp; }
-	public float getRotTurret() { return rotTurret;}
-	public void setRotTurret(float rotTurret) { this.rotTurret = rotTurret; }
-	public float getRotBase() {	return rotBase;	}
-	public void setRotBase(float rotBase) { this.rotBase = rotBase;	}
-	public Texture getTexBase() { return texBase; }
-	public void setTexBase(Texture texBase) { this.texBase = texBase; }
-	public Texture getTexTurret() { return texTurret; }
-	public void setTexTurret(Texture texTurret) { this.texTurret = texTurret; }
-	public Vector2f getPosition() { return position;}
-	public void setPosition(Vector2f position) { this.position = position; }
-	public TankStats getStats() { return stats; }
 	
 }
